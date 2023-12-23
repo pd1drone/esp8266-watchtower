36935f361d6dbd88cb2f15a15ae21de26ae9a7bc
pd1drone <karlangelolim19@gmail.com>
2023-12-23 16:32:46 +0800

    added right and left sensors

--- watchtower.ino
+++ watchtower.ino
@@ -10,19 +10,41 @@
 #define WIFI_SSID "ngaling"  // ssid-wifi name
 #define WIFI_PASSWORD "1234567890"  // wif-password
 
+// front ultrasonic sensor
+// trig pin = D5
+// echo pin = D6
 const int FrontTrigPin = 14;
 const int FrontEchoPin = 12;
+
+// left ultrasonic sensor
+// trig pin = D7
+// echo pin = D0
+const int LeftTrigPin = 13;
+const int LeftEchoPin = 16;
+
+// right ultrasonic sensor
+// trig pin = D4
+// echo pin = D8
+const int RightTrigPin = 2;
+const int RightEchoPin = 15;
+
 // defines variables
 long Frontduration;
+long Leftduration;
+long Rightduration;
 
 float waterLevel = 0.0;
+float prevwaterLevel =0.0;
+float waterLevelfront = 0.0;
+float waterLevelright = 0.0;
+float waterLevelleft = 0.0;
 
 unsigned long lastTime = 0;
 unsigned long timerDelay = 3000;
 
 const char* serverName = "http://192.168.254.170:8090/history";
 
-// Create AsyncWebServer object on port 80
+// Create AsyncWebServer object on port 8088
 AsyncWebServer server(8088);
 
 const char index_html[] PROGMEM = R"rawliteral(
@@ -98,6 +120,8 @@ void loop() {
 }
 
 void GetWaterLevel(){
+
+  // FRONT ULTRASONIC SENSOR
   digitalWrite(FrontTrigPin, LOW); 
   delayMicroseconds(2);
 
@@ -109,13 +133,82 @@ void GetWaterLevel(){
   Frontduration = pulseIn(FrontEchoPin, HIGH);
 
   float frontdata = Frontduration * 0.034 / 2; 
-  float distanceInMeters = (frontdata/100);
+  float distanceInMetersF = (frontdata/100);
 
-  float distanceMapped = floatMap(distanceInMeters, 0.2, 1.2, 0.0, 5.0);
-  waterLevel = 18.5 - distanceMapped;
-  Serial.println(distanceInMeters);
+  float distanceMappedF = floatMap(distanceInMetersF, 0.2, 1.2, 0.0, 5.0);
+  waterLevelfront = 18.5 - distanceMappedF;
+
+
+  // LEFT ULTRASONIC SENSOR
+  digitalWrite(LeftTrigPin, LOW); 
+  delayMicroseconds(2);
+
+  digitalWrite(LeftTrigPin, HIGH);
+  delayMicroseconds(20);
+
+  digitalWrite(LeftTrigPin, LOW);
+
+  Leftduration = pulseIn(LeftEchoPin, HIGH);
+
+  float leftdata   = Leftduration * 0.034 / 2; 
+  float distanceInMetersL = (leftdata/100);
+
+  float distanceMappedL = floatMap(distanceInMetersL, 0.2, 1.2, 0.0, 5.0);
+  waterLevelleft = 18.5 - distanceMappedL;
+
+  // RIGHT ULTRASONIC SENSOR
+  digitalWrite(RightTrigPin, LOW); 
+  delayMicroseconds(2);
+
+  digitalWrite(RightTrigPin, HIGH);
+  delayMicroseconds(20);
+
+  digitalWrite(RightTrigPin, LOW);
+
+  Rightduration = pulseIn(RightTrigPin, HIGH);
+
+  float rightdata    = Rightduration * 0.034 / 2; 
+  float distanceInMetersR = (rightdata /100);
+
+  float distanceMappedR = floatMap(distanceInMetersR, 0.2, 1.2, 0.0, 5.0);
+  waterLevelright = 18.5 - distanceMappedR;
+
+  float tolerance = 0.3; // Adjust the tolerance value as needed
+
+  // Check if the three sensor values are close to each other
+  if (abs(waterLevelfront - waterLevelleft) <= tolerance &&
+      abs(waterLevelfront - waterLevelright) <= tolerance &&
+      abs(waterLevelleft - waterLevelright) <= tolerance) {
+    // All three sensor values are close to each other
+
+    // Calculate the average of the three sensor values
+    waterLevel = (waterLevelfront + waterLevelleft + waterLevelright) / 3.0;
+  } else {
+    // At least one sensor value is not close to the others
+
+    // Check which two sensors are closer to each other
+    if (abs(waterLevelfront - waterLevelleft) <= tolerance &&
+        abs(waterLevelfront - waterLevelright) <= tolerance) {
+      // Front and Left sensors are close, average them
+      waterLevel = (waterLevelfront + waterLevelleft) / 2.0;
+    } else if (abs(waterLevelleft - waterLevelright) <= tolerance) {
+      // Left and Right sensors are close, average them
+      waterLevel = (waterLevelleft + waterLevelright) / 2.0;
+    } else if (abs(waterLevelfront - waterLevelright) <= tolerance) {
+      // Front and Right sensors are close, average them
+      waterLevel = (waterLevelfront + waterLevelright) / 2.0;
+    } else {
+      // None of the sensors are close to each other, choose one arbitrarily
+      waterLevel = prevwaterLevel;
+    }
+  }
+
+  // Set prevwaterLevel to the current waterLevel
+  prevwaterLevel = waterLevel;
+
   delay(100);
 }
+
 
 
 
